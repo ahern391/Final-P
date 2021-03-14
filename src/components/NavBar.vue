97b6190c36a67910189332f622d8c1266ef67063
ahern391 <77907451+ahern391@users.noreply.github.com>
2021-03-14 04:18:15 -0500

    added login

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,31 +1,44 @@
 <template>
-  <div class="navbar mb-2 shadow-lg bg-neutral text-neutral-content">
+  <Alert v-if="isError" />
+  <div class="navbar mb-2 shadow-lg bg-content-800 text-content-100">
     <div class="flex-1 px-2 mx-2">
       <span class="text-lg font-bold"> LoginExample </span>
     </div>
-    <div class="flex-none hidden px-2 mx-2 lg:flex">
+    <div class="flex-none flex px-2 mx-2">
       <div class="flex">
-        <router-link to="/" class="btn btn-ghost btn-sm rounded-btn">
+        <router-link
+          to="/"
+          class="btn btn-ghost btn-sm rounded-btn hover:text-content-100"
+        >
           Home
         </router-link>
-        <router-link
-          v-if="!isAuthenticated"
-          to="/Login"
-          class="btn btn-ghost btn-sm rounded-btn"
-        >
-          Login
-        </router-link>
+        <div v-if="!isAuthenticated">
+          <router-link
+            to="/login"
+            class="btn btn-ghost btn-sm rounded-btn hover:text-content-100"
+          >
+            Login
+          </router-link>
+          <router-link
+            to="/signup"
+            class="btn btn-ghost btn-sm rounded-btn hover:text-content-100"
+          >
+            SignUp
+          </router-link>
+        </div>
         <div v-else>
-      <router-link to="/Secret" class="btn btn-ghost btn-sm rounded-btn">
-        Secret
-      </router-link>
-        <button
-          to="/Login"
-          class="btn btn-error btn-sm rounded-btn"
-          @click="logout"
-        >
-          Logout
-        </button>
+          <router-link
+            to="/secret"
+            class="btn btn-ghost btn-sm rounded-btn hover:text-content-100"
+          >
+            Secret Page
+          </router-link>
+          <button
+            class="btn btn-error btn-sm rounded-btn hover:text-content-100"
+            @click="logout"
+          >
+            Logout
+          </button>
         </div>
       </div>
     </div>
@@ -34,16 +47,11 @@
 
 <script setup>
 import { useRouter } from 'vue-router'
-import { isAuthenticated } from '../helpers/useAuth.js'
+import { isAuthenticated, signOut } from '../helpers/useAuth.js'
+import { isError } from '../helpers/useError'
 const router = useRouter()
-const logout = () => {
-  isAuthenticated.value = false
+const logout = async () => {
+  await signOut()
   router.push('/')
 }
 </script>
-
-<style>
-.router-link-active {
-  @apply btn-success;
-}
-</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,40 +1,49 @@
 <template>
-  <section class="container flex items-center justify-center text-center bg-content-800 rounded-xl mx-auto mt-24 max-w-md bg-coolGray-800 text-coolGray-100">
-    <div
-      class="container flex items-center justify-center text-center cc_cursor"
-    >
-      <form
-        @submit.prevent
-        class="flex flex-col w-full max-w-lg p-12 rounded shadow-lg"
+  <section
+    class="container flex items-center justify-center text-center bg-content-800 rounded-xl mx-auto mt-24 max-w-md"
+  >
+    <form @submit.prevent class="flex flex-col w-full p-12 shadow-lg">
+      <label
+        for="username"
+        class="self-start text-xs font-semibold text-content-200"
+        >Email</label
       >
-        <label for="username" class="self-start text-xs font-semibold"
-          >Username or Email</label
-        >
-        <input
-          id="username"
-          type="text"
-          class="flex items-center h-12 px-4 mt-2 rounded focus:outline-none focus:ring-2 text-coolGray-900"
-        />
-        <label for="password" class="self-start mt-3 text-xs font-semibold"
-          >Password</label
-        >
-        <input
-          id="password"
-          type="password"
-          class="flex items-center h-12 px-4 mt-2 rounded focus:outline-none focus:ring-2 text-coolGray-900"
-        />
-        <button @click="login" class="btn btn-primary mt-4">Login</button>
-      </form>
-    </div>
+      <input
+        id="username"
+        type="text"
+        class="flex items-center h-12 px-4 mt-2 bg-gray-200 rounded focus:outline-none focus:ring-2"
+        v-model="email"
+      />
+      <label
+        for="password"
+        class="self-start mt-3 text-xs font-semibold text-content-200"
+        >Password</label
+      >
+      <input
+        id="password"
+        type="password"
+        class="flex items-center h-12 px-4 mt-2 bg-gray-200 rounded focus:outline-none focus:ring-2"
+        v-model="password"
+      />
+      <button @click="login" class="btn btn-accent mt-8">Login</button>
+    </form>
   </section>
+  <Form />
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import { useRouter } from 'vue-router'
-import { isAuthenticated } from '../helpers/useAuth'
+import { signIn } from '../helpers/useAuth'
 const router = useRouter()
-const login = () => {
-  isAuthenticated.value = true
-  router.push('/')
+const login = async () => {
+  try {
+    await signIn(email.value, password.value)
+    router.push('/')
+  } catch (error) {
+    console.log(error)
+  }
 }
+const email = ref('')
+const password = ref('')
 </script>
